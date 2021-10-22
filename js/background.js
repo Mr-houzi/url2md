337427f325e4f007f1f36f12f73900c55bfd7160
mr-houzi <ghosthouzi@foxmail.com>
2021-10-23 00:10:25 +0800

    '增加本页面url转化，图片alt说明'

--- js/background.js
+++ js/background.js
@@ -1,24 +1,60 @@
 chrome.contextMenus.create({
     title: "复制Url为Markdown格式",
     contexts: ["all"],
-    onclick: function(clickData){
-        console.log(1)
-        console.log(clickData)
-        let md = ''
-        if (clickData.mediaType === 'image') {
-             md = `![](${clickData.srcUrl})`
-        } else if (clickData.linkUrl !== 'undefined') {
-             md = `[](${clickData.linkUrl})`
-        }
-
-        // 复制到粘贴板
-        var input = document.createElement('textarea');
-        document.body.appendChild(input);
-        input.value = md;
-        input.focus();
-        input.select();
-        document.execCommand('Copy');
-        input.remove();
+    onclick: function(clickData, tab){
 
     }
 });
+
+// 复制到粘贴板
+function copy(text) {
+    var input = document.createElement('textarea');
+    document.body.appendChild(input);
+    input.value = text;
+    input.focus();
+    input.select();
+    document.execCommand('Copy');
+    input.remove();
+}
+
+chrome.contextMenus.onClicked.addListener((clickData, tab) => {
+    chrome.tabs.executeScript(tab.id, {
+        code: `(${findElement})(${JSON.stringify(clickData)})`,
+        frameId: clickData.frameId,
+        matchAboutBlank: true,
+        runAt: 'document_start',
+    }, ([result] = []) => {
+        if (chrome.runtime.lastError) {
+            console.error('Error: ' + chrome.runtime.lastError.message);
+        } else {
+
+            let md = ''
+            if (clickData.mediaType === 'image') {
+                let altText = typeof(result) == 'undefined' ? '' : result
+                md = `![${altText}](${clickData.srcUrl})`
+            } else if (typeof(clickData.linkUrl) !== 'undefined') {
+                let text = typeof(clickData.selectionText) == 'undefined' ? '未选中文字' : clickData.selectionText
+                md = `[${text}](${clickData.linkUrl})`
+            } else {
+                md = `[${tab.title}](${tab.url})`
+            }
+
+            // alert(md)
+            copy(md)
+        }
+    });
+});
+/**
+ * ***********************************************************
+ * this function's code will be executed as a content script in the web page
+ * @link https://stackoverflow.com/questions/61771600/is-there-a-way-to-get-the-alt-tag-of-an-image-from-chrome-contextmenu-create
+ */
+function findElement({mediaType, srcUrl}) {
+    const tagName = mediaType === 'image' ? 'img' : mediaType;
+    for (const el of document.querySelectorAll(tagName)) {
+        if (el.src === srcUrl) {
+            return el.alt;
+        }
+    }
+}
+/*************************** end ****************************/
